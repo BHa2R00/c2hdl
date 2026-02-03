814f5a5dc906dc259ba24a255c256fb734eb3ae2
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2026-02-03 21:31:19 +0800

    Add files via upload

--- include/stdio.h
+++ include/stdio.h
@@ -4,21 +4,6 @@ extern char stdin;
 #define stdout ((void*)&stdout)
 #define stdin ((void*)&stdin)
 
-void *memcpy(void *dest, const void *src, size_t n) {
-    char *d = (char *)dest;
-    const char *s = (const char *)src;
-    while (n--) *d++ = *s++;
-    return dest;
-}
-
-void *memset(void *s, int c, size_t n) {
-    unsigned char *p = (unsigned char *)s;
-    while (n--) {
-        *p++ = (unsigned char)c;
-    }
-    return s;
-}
-
 int __mulsi3(short a, short b) 
 {
   int m = 0;
--- include/string.h
+++ include/string.h
@@ -1,5 +1,22 @@
+
+void *memcpy(void *dest, const void *src, size_t n) {
+    char *d = (char *)dest;
+    const char *s = (const char *)src;
+    while (n--) *d++ = *s++;
+    return dest;
+}
+
+void *memset(void *s, int c, size_t n) {
+    unsigned char *p = (unsigned char *)s;
+    while (n--) {
+        *p++ = (unsigned char)c;
+    }
+    return s;
+}
+
 size_t strlen(const char *s) {
     size_t len = 0;
     while (s[len]) len++;
     return len;
 }
+
