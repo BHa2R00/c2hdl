25f3b1f1391c5722f12150e3861f4eea12df9532
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2026-02-05 13:25:03 +0800

    Add files via upload

--- rtl/ram.sv
+++ rtl/ram.sv
@@ -14,24 +14,20 @@ module ram
    input            rstb, clk
 );
 
-//(* ramstyle = "M9K" *) 
-//(* ram_style = "block" *) 
+(* ram_style = "block" *) 
+(* ramstyle = "M9K" *) 
 reg [31:0] mem[0:(SIZE>>2)-1];
 wire [31:0] addr1 = addr>>2;
-wire [3:0] be = (size==2 ? 4'b1111 : size==1 ? 4'b0011 : 4'b0001) << addr[1:0];
 wire [31:0] wdata1 = wdata << (8*addr[1:0]);
-reg [31:0] rdata1;
+wire [31:0] mask = (size==2 ? 32'hffffffff : size==1 ? 32'hffff : 32'hff) << (addr[1:0]*8);
+wire [31:0] unmask =~mask;
+wire [31:0] rdata1 = mem[addr1];
+wire [31:0] wdata2 = (wdata1 & mask) | (rdata1 & unmask);
 initial begin $readmemh(MEMH,mem); end
 always@(posedge clk) begin
   if(valid && write) begin
-    if(be[0]) mem[addr1][ 7: 0] <= wdata1[ 7: 0];
-    if(be[1]) mem[addr1][15: 8] <= wdata1[15: 8];
-    if(be[2]) mem[addr1][23:16] <= wdata1[23:16];
-    if(be[3]) mem[addr1][31:24] <= wdata1[31:24];
+    mem[addr1] <= wdata2;
   end
-  rdata1 <= mem[addr1];
-end
-always@(*) begin
   rdata = rdata1 >> (8*addr[1:0]);
 end
 
